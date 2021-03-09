ebd1e6d40281651e88110553bc85ea755bd6e45c
Mizanur Rahman <mizzanur007@gmail.com>
2021-03-09 14:54:35 +0600

    two ways of navigating dynamic route

--- src/components/Friend/Friend.js
+++ src/components/Friend/Friend.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./Friend.css";
 const Friend = (props) => {
   const { name, email, id } = props.friend;
+  const history = useHistory();
+
+  const handleClick = (friendId) => {
+    history.push(`/friend/${friendId}`);
+  };
   return (
     <div className="friend-container">
       <h3>Name : {name}</h3>
       <p>Email : {email}</p>
-      <p>
+      {/* <p>
         Id : {id} <Link to={`/friend/${id}`}> Show Details of Friend</Link>
-      </p>
+      </p> */}
+      {/* way one to navigate dynamic data */}
+      <button onClick={() => handleClick(id)}>Get Friend Info</button>
+      {/* way two to navigate dynamic data */}
     </div>
   );
 };
--- src/components/FriendDetail.js/FriendDetail.js
+++ src/components/FriendDetail.js/FriendDetail.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const FriendDetail = () => {
+  const [getFriend, setFriend] = useState({});
   const { friendId } = useParams();
+  useEffect(() => {
+    const url = `http://jsonplaceholder.typicode.com/users/${friendId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setFriend(data));
+  }, []);
   return (
     <div>
-      <h1>This is friend detail {friendId}</h1>
+      <h3>Name: {getFriend.name}</h3>
+      <h3>Email: {getFriend.email}</h3>
     </div>
   );
 };
