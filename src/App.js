33d4008ed8d5abb649c574ff842964f43b8030bd
Mizanur Rahman <mizzanur007@gmail.com>
2021-03-16 14:46:50 +0600

    add search functionality in react

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import MealDetail from "./components/MealDetail/MealDetail";
+import MealFinder from "./components/MealFinder/MealFinder";
 
 function App() {
   return (
     <div>
       <MealDetail></MealDetail>
+      <MealFinder></MealFinder>
     </div>
   );
 }
---
+++ src/components/MealFinder/MealFinder.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+
+const MealFinder = () => {
+  const [search, setSearch] = useState("");
+  const [meals, setMeals] = useState("");
+
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setSearch(event.target.value);
+  };
+
+  useEffect(() => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setMeals(data.meals));
+  }, [search]);
+
+  return (
+    <div>
+      <h1>Find Delicious Food</h1>
+      <input type="text" onChange={handleChange} placeholder="search food" />
+      <p>searching : {search}</p>
+      <p>meal found : {meals?.length || 0}</p>
+      <ul>
+        {meals?.map((meal) => (
+          <li>{meal.strMeal}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default MealFinder;
