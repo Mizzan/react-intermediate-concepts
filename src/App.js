1f1439f2e75e4b77672408845549901cc53238f5
Mizanur Rahman <mizzanur007@gmail.com>
2021-03-16 13:58:12 +0600

    Optional chaning for objects in react or js in general

--- src/App.js
+++ src/App.js
@@ -1,32 +1,10 @@
 import "./App.css";
-import { ThumbUp, ThumbDown } from "@material-ui/icons";
-import { useState } from "react";
+import MealDetail from "./components/MealDetail/MealDetail";
 
 function App() {
-  const [likeColor, setLikeColor] = useState("");
-  const [disLikeColor, setdisLikeColor] = useState("");
-
-  const handleLike = () => {
-    const color = likeColor ? "" : "primary";
-    setLikeColor(color);
-  };
-  const handleDisLike = () => {
-    const color = disLikeColor ? "" : "secondary";
-    setdisLikeColor(color);
-  };
-
   return (
     <div>
-      <ThumbUp
-        onClick={handleLike}
-        color={likeColor}
-        style={{ fontSize: 60 }}
-      ></ThumbUp>
-      <ThumbDown
-        onClick={handleDisLike}
-        color={disLikeColor}
-        style={{ fontSize: 60 }}
-      ></ThumbDown>
+      <MealDetail></MealDetail>
     </div>
   );
 }
